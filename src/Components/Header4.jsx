810eb9adebf379c2284b921bfc6e70503eeb8354
Nikhil Barasker <nikhilbaraskar20.nb@gmail.com>
2024-04-04 16:22:30 +0530

    new

--- src/Components/Header4.jsx
+++ src/Components/Header4.jsx
@@ -31,6 +31,10 @@ export default function Header4() {
     document.getElementById("search").value = "";
   };
 
+  const handleLoginClick = () => {
+    navigate('/login')
+  }
+
   const handleLinkClick = () => {
     window.scrollTo(0, 0);
   };
@@ -73,24 +77,6 @@ export default function Header4() {
               />
             </a>
             <div className="absolute sm:right-10 right-4 flex items-center">
-              {/* <abbr title="Wishlist">
-              <span className="relative sm:mr-8 mr-6">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20px"
-                  className="cursor-pointer fill-[#333] hover:fill-[#007bff] inline-block"
-                  viewBox="0 0 64 64"
-                >
-                  <path
-                    d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z"
-                    data-original="#000000"
-                  />
-                </svg>
-                <span className="absolute left-auto -ml-1 top-0 rounded-full bg-black px-1 py-0 text-xs text-white">
-                  1
-                </span>
-              </span>
-            </abbr> */}
               <span className="relative sm:mr-8 mr-6">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -135,6 +121,7 @@ export default function Header4() {
                     width="20px"
                     height="20px"
                     viewBox="0 0 24 24"
+                    onClick={()=>handleLoginClick()}
                     className="hover:fill-[#007bff]"
                   >
                     <circle cx="10" cy="7" r="6" data-original="#000000" />
